2e388300f8fc552c55fe00282f8b00afc729a230
junayed0011 <junayedbin11@gmail.com>
2026-05-19 16:46:49 +0600

    Initial commit

---
+++ bin/dart_variable.dart
@@ -0,0 +1,29 @@
+void main(){
+
+  var x = 10;
+  var y = 40;
+  var z = x+y;
+
+  print(z);
+
+  var postIncreament = (x++);
+
+  print(postIncreament);
+
+  var preIncreament = (++x);
+
+  print(preIncreament);
+
+  var postDecreament = (x--);
+
+  print(postDecreament);
+
+  var preDecreament = (--x);
+
+  print(preDecreament);
+
+  var string = 'My name is Junayed';
+
+  print(string);
+
+}
---
+++ bin/list_data_type.dart
@@ -0,0 +1,36 @@
+void main(){
+
+  var myList = [1,2,3,4,5,6];
+  print(myList);
+
+  var stringList = ['Junayed','232-15-450','CSE'];
+  print(stringList);
+
+  print(stringList[0]);
+
+//properties...
+  var result = stringList.isEmpty;
+  print(result);
+
+  var result2 = stringList.isNotEmpty;
+  print(result2);
+
+  var result3 = stringList.last;
+  print(result3);
+
+  stringList.add('DIU');
+  print(stringList);
+
+  //fixed length list...
+  const myData = [25,30,45];
+  print(myData);
+
+  //growable list(List insert)...
+  var numberList = [34,77,108];
+  numberList.addAll([207,355]);
+  print(numberList);
+  
+  var numberList2 = [1,3,4,6,9];
+  print(object)
+
+}

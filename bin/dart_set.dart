1989ae245a81b07d0a2246af4adc857a5ba4b924
junayed0011 <junayedbin11@gmail.com>
2026-05-20 17:55:23 +0600

    update

--- bin/dart_set.dart
+++ bin/dart_set.dart
@@ -1,5 +1,15 @@
 void main(){
 
-  var names = <String>{'Junayed','Emon',''};
+  var names = <String>{'Junayed','Emon','Tajin'};
+
+  names.add('Dhaka');
+
+  print(names);
+
+
+  var numbers = <int>{1,3,5,8};
+  numbers.addAll({9,10,15});
+
+  print(numbers);
 
 }

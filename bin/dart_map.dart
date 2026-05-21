fbf94a412d4ae7c4cbd1542d9e82f83549ac72cf
junayed0011 <junayedbin11@gmail.com>
2026-05-21 23:13:21 +0600

    update

--- bin/dart_map.dart
+++ bin/dart_map.dart
@@ -1,7 +1,41 @@
 void main(){
 
   var person = {
-    'Name': 'Junayed'
+    'Name': 'Junayed',
+    'Age': '24',
+    'City': 'Nilphamari',
   };
+
+  print(person);
+
+  //add new element on map...
+  person['Country'] = 'Bangladesh';
+  print(person);
+
+  //using map constructor...
+  var details = new Map();
+  details['Name'] = 'Junayed';
+  details['Age'] = '24';
+  details['City'] = 'Nilphamari';
+
+  //map constructor properties..
+  print(details.keys);
+  print(details.values);
+  print(details.length);
+  print(details.isEmpty);
+  print(details.isNotEmpty);
+
+  //Map methods...
+  //addAll method..
+  person.addAll({'Blood group':'A+', 'Region':'Islam'});
+  print(person);
+
+  //clear method..
+  person.clear();
+  print(person);
+
+  //remove method..
+  person.remove('Name');
+  print(person);
 
 }
